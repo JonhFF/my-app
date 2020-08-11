fd981a29ccfc4fb301acbf3eb785e8f19b04db5d
Jcnx21 <jcnx21@gmail.com>
2020-08-10 18:26:21 -0600

    "Seccion 7"

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
+import withClass from '../../hoc/withClass';
 
-const cockpit = ( props ) => {
-    const assignedClasse = [];
-    let btnClases = '';
-    if(props.showPersons) {
-        btnClases = classes.Red;
-    }
+const Cockpit = props => {
+  const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
 
-    if( props.persons.length <= 2){
-        assignedClasse.push( classes.Red );
-    }
-    if( props.persons.length <= 1){
-        assignedClasse.push( classes.bold );
-    }
+  console.log(authContext.authenticated);
 
-    return (
-        <div className={classes.cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasse.join(' ')}>This is really working</p>
-            <button 
-                className={btnClases}
-                onClick={props.clicked}>
-                Toggle Persons
-                </button>
-        </div>
-    )
-}
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request...                                                                                       
+    // setTimeout(() => {
+    //   alert('Saved data to cloud!');
+    // }, 1000);
+    toggleBtnRef.current.click();
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
 
-export default cockpit;
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
+
+  // useEffect();
+
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+
+  if (props.personsLength <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.personsLength <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+      <button onClick={authContext.login}>Log in</button>
+    </div>
+  );
+};
+
+export default withClass(Cockpit, classes.Cockpit);
